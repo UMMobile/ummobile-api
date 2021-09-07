1a2e375852a31d653a6282d931c4eb29f77e85f8
jonathangomz <jonicgp97@gmail.com>
2021-09-07 12:37:01 -0500

    Remove unused imports

--- src/catalogue/catalogue.module.ts
+++ src/catalogue/catalogue.module.ts
@@ -3,9 +3,7 @@ import { CatalogueService } from './catalogue.service';
 import { CatalogueController } from './catalogue.controller';
 import { HttpModule } from '@nestjs/axios';
 import { LoginModule } from 'src/login/login.module';
-import { UtilsService } from 'src/utils/utils.service';
 import { UtilsModule } from 'src/utils/utils.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [UtilsModule, LoginModule, HttpModule],
