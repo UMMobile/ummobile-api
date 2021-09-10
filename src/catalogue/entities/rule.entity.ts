234d8fcf4812427ff7dc4e0f5dc0f62662d36eee
jonathangomz <jonicgp97@gmail.com>
2021-09-10 11:37:28 -0500

    Remove name field from rule class

--- src/catalogue/entities/rule.entity.ts
+++ src/catalogue/entities/rule.entity.ts
@@ -3,9 +3,5 @@ import { Roles } from "src/statics/roles.enum";
 export class Rule {
   roles: Roles[];
   keyName: String;
-  name: {
-    es: String;
-    en: String;
-  };
   pdfUrl: String;
 }
--- src/statics/rules.list.ts
+++ src/statics/rules.list.ts
@@ -5,37 +5,21 @@ export const rules: Rule[] = [
   {
     roles: [Roles.Student, Roles.Employee],
     keyName: 'student_rules',
-    name: {
-      en: 'Something',
-      es: 'Algo',
-    },
     pdfUrl: 'https://um.edu.mx/descargas/reglamento.pdf',
   },
   {
     roles: [Roles.Student, Roles.Employee],
     keyName: 'legislation_undergraduate',
-    name: {
-      en: 'Something',
-      es: 'Algo',
-    },
     pdfUrl: 'https://um.edu.mx/descargas/leg_pregrado.pdf',
   },
   {
     roles: [Roles.Student, Roles.Employee],
     keyName: 'legislation_posgraduate',
-    name: {
-      en: 'Something',
-      es: 'Algo',
-    },
     pdfUrl: 'https://um.edu.mx/descargas/leg_posgrado.pdf',
   },
   {
     roles: [Roles.Employee],
     keyName: 'policy_manual',
-    name: {
-      en: 'Something',
-      es: 'Algo',
-    },
     pdfUrl: 'https://virtual-um.um.edu.mx/financiero/rh/formularios/ManualDePoliticas.pdf',
   },
 ];
