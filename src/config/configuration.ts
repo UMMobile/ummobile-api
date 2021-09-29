c6accf92d4018f5c1629f817ff71ecea5734da05
jonathangomz <jonicgp97@gmail.com>
2021-09-29 11:45:18 -0500

    Add comments

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -8,14 +8,18 @@ import identityServerConfig from './identityServer.config';
 export default ConfigModule.forRoot({
     load: [academicConfig, apiManagerConfig, databaseConfig, identityServerConfig],
     validationSchema: Joi.object({
+        // Academic
         ACADEMIC_URL: Joi.string().required(),
         ACADEMIC_USER: Joi.string().required(),
         ACADEMIC_PASSWORD: Joi.string().required(),
+        // API Manager
         AM_URL: Joi.string().required(),
         AM_API_KEY: Joi.string().required(),
+        // Identity Server
         IS_URL: Joi.string().required(),
         IS_USER: Joi.string().required(),
         IS_PASSWORD: Joi.string().required(),
+        // Database
         DATABASE_URI: Joi.string().required(),
     }),
     isGlobal: true,
