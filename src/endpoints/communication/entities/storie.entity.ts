d371aa270fc10c83c74fd2ff70ec3ff4df4553f1
jonathangomz <jonicgp97@gmail.com>
2021-10-21 11:26:53 -0500

    Fixes enum type for Swagger docs

--- src/endpoints/communication/entities/storie.entity.ts
+++ src/endpoints/communication/entities/storie.entity.ts
@@ -1,9 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { MediaType } from "src/statics/mediaType.enum";
 
 export class Story {
   startDate: Date;
   endDate: Date;
   duration: number;
+  @ApiProperty({
+    enum: MediaType,
+  })
   type: MediaType;
   content: string;
 }
